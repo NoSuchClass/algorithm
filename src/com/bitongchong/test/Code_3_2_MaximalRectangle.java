d49ee9813fbf04f0a1a19b180f280ed311855fc7
NoSuchClass <a343788619@163.com>
2019-05-24 08:25:42 +0800

    提交

---
+++ src/com/bitongchong/test/Code_3_2_MaximalRectangle.java
@@ -0,0 +1,14 @@
+package com.bitongchong.test;
+
+public class Code_3_2_MaximalRectangle {
+    public static int maxRecSize(int[][] map) {
+        int max = 0;
+        for (int i = 0; i < map.length; i++) {
+            int[] arr = new int[map[0].length];
+            for (int j = 0; j < map[0].length; j++) {
+                arr[j] = map[i][j] == 0 ? 0 : arr[j] + 1;
+            }
+
+        }
+    }
+}
