59031f62c1197959b5ed7b05b3fe1ef81a6621ed
liuyuehe <liuyuehe09379@hellobike.com>
2019-07-31 11:34:41 +0800

    二维数组中查找一个数

--- src/com/bitongchong/codinginterviews/Code_01_ArrayNumberFind.java
+++ src/com/bitongchong/codinginterviews/Code_01_ArrayNumberFind.java
@@ -6,7 +6,7 @@ package com.bitongchong.codinginterviews;
  * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
  * 判断数组中是否含有该整数。
  */
-public class Code_00_ArrayNumberFind {
+public class Code_01_ArrayNumberFind {
     public boolean Find(int target, int[][] array) {
         int n = array[0].length - 1;
         int m = 0;
