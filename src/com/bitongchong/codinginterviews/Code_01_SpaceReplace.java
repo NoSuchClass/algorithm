5272faac8c02c8e1dfd05de8a68390c789a7d464
liuyuehe <liuyuehe09379@hellobike.com>
2019-07-31 11:11:33 +0800

    空格替换

--- src/com/bitongchong/codinginterviews/Code_01_SpaceReplace.java
+++ src/com/bitongchong/codinginterviews/Code_01_SpaceReplace.java
@@ -6,7 +6,7 @@ package com.bitongchong.codinginterviews;
  */
 public class Code_01_SpaceReplace {
     public static String replaceSpace(StringBuffer str) {
-		 if(str==null){
+/*		 if(str==null){
 		    return null;
 		 }
 		 for(int i=0;i<str.length();i++){
@@ -15,11 +15,30 @@ public class Code_01_SpaceReplace {
 		    str.replace(i,i+1,"%20");
 		     }
 		  }
-		  return str.toString();   
-    }
+		  return str.toString();   */
+		char[] chars = str.toString().toCharArray();
+		int count = 0;
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == ' ') {
+				count++;
+			}
+		}
+		char[] newChars = new char[count * 2 + chars.length];
+		int n = newChars.length - 1;
+		for (int i = chars.length - 1; i >= 0 ; i--) {
+			if (chars[i] == ' ') {
+				newChars[n--] = '0';
+				newChars[n--] = '2';
+				newChars[n--] = '%';
+			} else {
+				newChars[n--] = chars[i];
+			}
+		}
+		return new String(newChars);
+	}
  
     public static void main(String[] args) {
-    	StringBuffer str = new StringBuffer("a a a a");
+    	StringBuffer str = new StringBuffer("12 3a3 a a");
     	String str1 = replaceSpace(str);
     	System.out.println(str1);
 	}
