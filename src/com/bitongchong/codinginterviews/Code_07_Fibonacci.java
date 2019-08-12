958673646433a097cd8e30d53fc13f478942ba8a
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-12 14:04:44 +0800

    斐波拉契

--- src/com/bitongchong/codinginterviews/Code_07_Fibonacci.java
+++ src/com/bitongchong/codinginterviews/Code_07_Fibonacci.java
@@ -1,12 +1,14 @@
 package com.bitongchong.codinginterviews;
 
-/*
+/**
+ * @author boot liu
+ * @description
  * 题目描述：
-大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
-n<=39
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+ * n<=39
  */
 public class Code_07_Fibonacci {
-    public int Fibonacci(int n) {
+    public int fibonacci(int n) {
         if (n < 2) {
             return n;
         }
