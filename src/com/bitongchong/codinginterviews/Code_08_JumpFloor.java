4bcd29206977f9d6d4d5574227c1cfe2894e7f2c
liuyuehe <liuyuehe09379@hellobike.com>
2019-11-15 15:24:37 +0800

    跳楼梯

---
+++ src/com/bitongchong/codinginterviews/Code_08_JumpFloor.java
@@ -0,0 +1,22 @@
+package com.bitongchong.codinginterviews;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author liuyuehe
+ * @description 题目描述
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶
+ * 总共有多少种跳法（先后次序不同算不同的结果）。
+ * @date 2019/8/12
+ */
+public class Code_08_JumpFloor {
+    public static void main(String[] args) {
+        int i = 20;
+        String string = new String();
+        List<Integer> list = new ArrayList<>(16);
+
+    }
+}
