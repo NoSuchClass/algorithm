6621ce14af4cd7511f4faf60fd13ab652fda8749
NoSuchClass <a343788619@163.com>
2020-04-17 20:56:45 +0800

    LeetCode062-不同路径

---
+++ src/main/java/com/bitongchong/leetcode/Code_062_UniquePaths.java
@@ -0,0 +1,23 @@
+package com.bitongchong.leetcode;
+
+/**
+ * @author liuyuehe
+ * @date 2020/4/17 17:51
+ */
+public class Code_062_UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            grid[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            grid[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
