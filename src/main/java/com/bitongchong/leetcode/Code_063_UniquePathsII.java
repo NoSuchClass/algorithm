6156b5f69696e6a39ceefcb0686c6fa17b581df8
NoSuchClass <a343788619@163.com>
2020-04-18 21:26:38 +0800

    update:LeetCode063-不同路径II

--- src/main/java/com/bitongchong/leetcode/Code_063_UniquePathsII.java
+++ src/main/java/com/bitongchong/leetcode/Code_063_UniquePathsII.java
@@ -11,6 +11,8 @@ public class Code_063_UniquePathsII {
         int[][] grid = new int[m][n];
         if (obstacleGrid[m - 1][n - 1] == 1) {
             return 0;
+
+
         }
         grid[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
         for (int i = 1; i < m; i++) {
@@ -43,5 +45,64 @@ public class Code_063_UniquePathsII {
             }
         }
         return grid[m - 1][n - 1];
+    }
+
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] arr = new int[n];
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            }
+            arr[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 0 && j != 0) {
+                    arr[j] += arr[j - 1];
+                }
+                if (obstacleGrid[i][j] != 0) {
+                    arr[j] = 0;
+                }
+            }
+        }
+        return arr[n - 1];
+    }
+
+    /**
+     * 因为给了个数组，可以直接使用，优化空间复杂度O(1)
+     * @param obstacleGrid -
+     * @return -
+     */
+    public int uniquePathsWithObstacles3(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        obstacleGrid[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
+        }
+        for (int i = 1; i < m; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i][j - 1] + obstacleGrid[i - 1][j];
+                } else {
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+        return obstacleGrid[m - 1][n - 1];
+    }
+    public static void main(String[] args){
+        System.out.println(new Code_063_UniquePathsII().uniquePathsWithObstacles3(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
     }
 }
