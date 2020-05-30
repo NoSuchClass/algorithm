54d0478780f5d82f6859fb4a2ee0fca564dd3b38
NoSuchClass <a343788619@163.com>
2020-05-30 13:39:35 +0800

    fix-LeetCode309-买卖股票的最佳时机含冷冻期

--- src/main/java/com/bitongchong/leetcode/Code_309_BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/com/bitongchong/leetcode/Code_309_BestTimeToBuyAndSellStockWithCooldown.java
@@ -10,8 +10,8 @@ public class Code_309_BestTimeToBuyAndSellStockWithCooldown {
             return 0;
         }
         int dp_i_0 = 0;
-        int dp_i_1 = -prices[0];
-        int dp_pre_0 = Integer.MIN_VALUE >> 1;
+        int dp_i_1 = Integer.MIN_VALUE >> 1;
+        int dp_pre_0 = 0;
         for (int i = 0; i < prices.length; i++) {
             int temp = dp_i_0;
             dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
