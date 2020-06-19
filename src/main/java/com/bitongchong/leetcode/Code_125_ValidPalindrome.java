93660bc05bce47d94bf7690dcbe27698b23f2d5f
NoSuchClass <a343788619@163.com>
2020-06-19 11:24:27 +0800

    LeetCode125-验证回文串

---
+++ src/main/java/com/bitongchong/leetcode/Code_125_ValidPalindrome.java
@@ -0,0 +1,37 @@
+package com.bitongchong.leetcode;
+
+/**
+ * @author liuyuehe
+ * @date 2020/6/19 9:49
+ */
+public class Code_125_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if (s.length() <= 1) {
+            return true;
+        }
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if ((s.charAt(left) | 0x20) != (s.charAt(right) | 0x20) && left < right) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean valid(char c) {
+        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Code_125_ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
